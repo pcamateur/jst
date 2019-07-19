6c7581365fe2e49ba92dff8e1c5b26f66147c29f
pcamateur <pcamateur@outlook.com>
2019-07-19 17:21:40 +0800

    update

--- waybill/bill.js
+++ waybill/bill.js
@@ -1,6 +1,19 @@
+// function programming() {
+//     var originalValue = document.getElementById('inputa').value;
+//     var programmingValue = eval(originalValue) % 7;
+//     var eValue = originalValue + programmingValue;
+//     document.getElementById("inputa").innerHTML = eValue;
+// }
+
+var originalValue;
+originalValue = document.getElementById('inputa').value;
+
+var programmingValue;
+programmingValue = originalValue % 7;
+
+var eValue;
+eValue = originalValue + programmingValue;
+
 function programming() {
-    var originalValue = document.getElementById('inputa').value;
-    var programmingValue = eval(originalValue) % 7;
-    var eValue = originalValue + programmingValue;
-    document.getElementById("inputa").innerHTML = eValue;
+    document.getElementById('values').value = eValue;
 }
